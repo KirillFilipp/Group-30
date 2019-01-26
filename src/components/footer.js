b3b5ebe438690954ba9d26c483cd5c13249ff6e2
Katsiaryna Mazgo <ekaterina.mozgo@mail.ru>
2019-01-26 21:28:41 +0300

    fix: fix bug with images

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,50 +1,66 @@
-const footer = `
+function getImagesPath() {
+  const url = window.location.href;
+  const arr = url.split('/');
+  arr.pop();
+  const lastElem = arr[arr.length - 1];
+
+  if (lastElem === 'search' || lastElem === 'person' || lastElem === 'home') {
+    arr.pop();
+    arr.pop();
+  }
+  return arr.join('/');
+}
+
+function getFooterHtml() {
+  const imagesPath = getImagesPath();
+  return `
   <div class="container">
     <p class="project-authors text-center">Авторы проекта</p>
     <div class="row">
       <div class="col-lg-2 col-md-4 col-sm-6 project-author">
         <figure>
-          <img class="img-project-author" src="./imagesComponents/kirillfilipp.jpeg" alt="kirillfilipp">
+          <img class="img-project-author" src="${imagesPath}/imagesComponents/kirillfilipp.jpeg" alt="kirillfilipp">
           <figcaption>GitHub: <a href="https://github.com/kirillfilipp" target="_blank">kirillfilipp</a></figcaption>
         </figure>
       </div>
       <div class="col-lg-2 col-md-4 col-sm-6 project-author">
         <figure>
-          <img class="img-project-author" src="./imagesComponents/mozgoen.jpeg" alt="mozgoen">
+          <img class="img-project-author" src="${imagesPath}/imagesComponents/mozgoen.jpeg" alt="mozgoen">
           <figcaption>GitHub: <a href="https://github.com/mozgoen" target="_blank">mozgoen</a></figcaption>
         </figure>
       </div>
       <div class="col-lg-2 col-md-4 col-sm-6 project-author">
         <figure>
-          <img class="img-project-author" src="./imagesComponents/kozyritskiy.jpeg" alt="kozyritskiy">
+          <img class="img-project-author" src="${imagesPath}/imagesComponents/kozyritskiy.jpeg" alt="kozyritskiy">
           <figcaption>GitHub: <a href="https://github.com/kozyritskiy" target="_blank">kozyritskiy</a></figcaption>
         </figure>
       </div>
       <div class="col-lg-2 col-md-4 col-sm-6 project-author">
         <figure>
-          <img class="img-project-author" src="./imagesComponents/kulinar310.jpeg" alt="kulinar310">
+          <img class="img-project-author" src="${imagesPath}/imagesComponents/kulinar310.jpeg" alt="kulinar310">
           <figcaption>GitHub: <a href="https://github.com/kulinar310" target="_blank">kulinar310</a></figcaption>
         </figure>
       </div>
       <div class="col-lg-2 col-md-4 col-sm-6 project-author">
         <figure>
-          <img class="img-project-author" src="./imagesComponents/estel1591.jpeg" alt="estel1591">
+          <img class="img-project-author" src="${imagesPath}/imagesComponents/estel1591.jpeg" alt="estel1591">
           <figcaption>GitHub: <a href="https://github.com/estel1591" target="_blank">estel1591</a></figcaption>
         </figure>
       </div>
       <div class="col-lg-2 col-md-4 col-sm-6 project-author">
         <figure>
-          <img class="img-project-author" src="./imagesComponents/niksrakevich.jpeg" alt="niksrakevich">
+          <img class="img-project-author" src="${imagesPath}/imagesComponents/niksrakevich.jpeg" alt="niksrakevich">
           <figcaption>GitHub: <a href="https://github.com/niksrakevich" target="_blank">niksrakevich</a></figcaption>
         </figure>
       </div>
     </div>
   </div>
 `;
+}
 
 export default function addFooter() {
   const footerElement = document.createElement('footer');
-  footerElement.innerHTML = footer;
+  footerElement.innerHTML = getFooterHtml();
   const body = document.querySelector('body');
   body.appendChild(footerElement);
 }
