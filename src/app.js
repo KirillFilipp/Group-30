8bc3d6d6ef8579a4c4484901ee640c57358bce6c
estel1591 <estel1591@mail.ru>
2019-01-27 14:08:03 +0300

    fix: add search function in app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/saveLanguageContext';
 import heelo from './pages/home/script';
-import heelo3 from './pages/search/script';
+import search from './pages/search/script';
 import addHeader from './components/header';
 import addFooter from './components/footer';
 import choiceLanguage from './components/translate';
